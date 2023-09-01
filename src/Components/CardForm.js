4c07380b926a6fe9b55a09823895ce02af7e1a11
Omar Hafez <omarhafez@Omars-MacBook-Pro-2.local>
2023-09-01 09:03:59 -0400

    fixed card add

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 function CardForm({card, deck, handleSubmit, cardId, handleDone, handleChange}) {
 
   // const [card, setCard] = useState(initialFormState)
